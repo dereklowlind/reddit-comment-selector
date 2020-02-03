8d79ee578f100d87c47e4fdca2dc39bb46919ab0
dereklowlind <dereklowlind@gmail.com>
2020-02-02 20:25:55 -0800

    Updates

---
+++ precache-manifest.d89bbcfd27bf1bf9a577fa7bc1dcfdff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82c68060bab35854c6889c0cd7ad68c",
+    "url": "/reddit-comment-selector/index.html"
+  },
+  {
+    "revision": "09d53519ed63822f7525",
+    "url": "/reddit-comment-selector/static/css/main.10cc3580.chunk.css"
+  },
+  {
+    "revision": "8bdd7199d07d0e5b58f5",
+    "url": "/reddit-comment-selector/static/js/2.8f1546aa.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/reddit-comment-selector/static/js/2.8f1546aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09d53519ed63822f7525",
+    "url": "/reddit-comment-selector/static/js/main.42242666.chunk.js"
+  },
+  {
+    "revision": "47485527eb9461436bba",
+    "url": "/reddit-comment-selector/static/js/runtime-main.7015a35f.js"
+  },
+  {
+    "revision": "2bf9d8461ce84d9f78cad1989e2d4ef8",
+    "url": "/reddit-comment-selector/static/media/reddit-uparrow.2bf9d846.png"
+  }
+]);
